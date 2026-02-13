f44dec4edfccf34c75811f8326e9628363a89d33
Krish Bharal <krishbharal@gmail.com>
2026-02-12 23:17:13 -0600

    Reverted the express to 4.21.0

--- index.js
+++ index.js
@@ -23,9 +23,10 @@ app.use(express.static(path.join(__dirname, "./client/dist")));
 console.log("Serving from:", path.join(__dirname, "client", "dist"));
 
 // Catch-all fallback for SPA
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/dist/index.html"));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
+
 const PORT = process.env.PORT || 3000;
 const rooms = new Map();
 
